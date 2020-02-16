d5addc11ca40165837a55d958f7ebe904745b5c8
devitasari <devitas700@gmail.com>
2020-02-16 22:32:50 +0700

    using randint

---
+++ gatcha.py
@@ -0,0 +1,14 @@
+import random
+from random import randint
+cube = randint(1,5)
+
+if cube == 1:
+    print "coba lagi ya"
+elif cube == 2:
+    print "selamat kamu mendapatkan kupon sebanyak 5"
+elif cube == 3:
+    print "selamat kamu mendapatkan kupon sebanyak 15"
+elif cube == 4:
+    print "selamat kamu mendapatkan kupon sebanyak 50"
+elif cube == 5:
+    print "WOW, kamu menang jackpot! Selamat!!"
