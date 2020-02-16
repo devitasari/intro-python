14f81ca773ccadbedcdf2498d22679b3c389d4fe
devitasari <devitas700@gmail.com>
2020-02-16 20:14:46 +0700

    basic string modification

---
+++ string.py
@@ -0,0 +1,25 @@
+#Let's Form a Sentence
+
+word = "JavaScript"
+second = "is"
+third = "awesome"
+fourth = "and"
+fifth = "I"
+sixth = "love"
+seventh = "it!"
+
+print word,second,third,fourth,fifth,sixth,seventh
+
+#Index Accessing -1 by 1
+word = 'wow JavaScript is so cool'
+exampleFirstWord = word[0:3]
+secondWord = word[4:14]
+thirdWord = word[15:17]
+fourthWord = word[18:20]
+fifthWord = word[21:25]
+
+print "First Word:", exampleFirstWord, "jumlah karakter:", len(exampleFirstWord)
+print "Second Word:", secondWord, "jumlah karakter:", len(secondWord)
+print "Third Word:", thirdWord, "jumlah karakter:", len(thirdWord)
+print "Fourth Word:", fourthWord, "jumlah karakter:", len(fourthWord)
+print "Fifth Word:", fifthWord, "jumlah karakter:", len(fifthWord)
