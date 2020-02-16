c3fd23263b9dd2a6dd9692b3cd4c911b51c6b482
devitasari <devitas700@gmail.com>
2020-02-16 22:58:46 +0700

    looping for and while

---
+++ ILoveCoding.py
@@ -0,0 +1,21 @@
+#for loop
+
+print "LOOPING FOR PERTAMA"
+for i in range(1,21,1):
+    print str(i)+" - I love coding"
+print "LOOPING FOR KEDUA"
+for i in range(20,0,-1):
+    print str(i)+" - I will become fullstack developer"
+
+#while loop
+
+print "LOOPING WHILE PERTAMA"
+count1 = 1
+while (count1 <= 20):
+    print str(count1)+" - I love coding"
+    count1 = count1 + 1
+print "LOOPING WHILE KEDUA"
+count2 = 20
+while (count2 > 0):
+    print str(count2)+" - I will become fullstack developer"
+    count2 = count2 - 1
