5ed57adf50b6e7322428c74833b32c0006b609ad
devitasari <devitas700@gmail.com>
2020-02-25 16:36:08 +0700

    add truty falsy

--- sorting.py
+++ sorting.py
@@ -6,4 +6,58 @@ def sorting(array):
                 [array[j-1], array[j]] = [array[j], array[j-1]]
     return array
 
-print(sorting([ 2, 4, 6, 8, 2, 3 ]))
+# print(sorting([ 2, 4, 6, 8, 2, 3 ]))
+
+#release 1
+def sortingByType(array):
+    numberList = []
+    stringList = []
+    boolList = []
+    for i in array:
+        if type(i) == int:
+            numberList.append(i)
+        for j in range(len(numberList)):
+            if (numberList[j] < numberList[j-1]):
+                [numberList[j-1],numberList[j]] =  [numberList[j],numberList[j-1]]
+        if type(i) == bool:
+            boolList.append(i)
+            for j in range(len(boolList)):
+                if (boolList[j] < boolList[j-1]):
+                    [boolList[j-1],boolList[j]] =  [boolList[j],boolList[j-1]]
+        if type(i) == str:
+            stringList.append(i)
+            for j in range(len(boolList)):
+                if (boolList[j] < boolList[j-1]):
+                    [boolList[j-1],boolList[j]] =  [boolList[j],boolList[j-1]]
+    return [numberList, stringList, boolList]         
+
+# print(sortingByType([ 1, 3, 'array', -45, True, False, 'big' ]))
+
+# release 2
+def sortAllClean(array):
+    numberList = []
+    stringList = []
+    boolList = []
+    for i in array:
+        if not i:
+            del i
+
+    for i in array:
+        if type(i) == int:
+            numberList.append(i)
+        for j in range(len(numberList)):
+            if (numberList[j] < numberList[j-1]):
+                [numberList[j-1],numberList[j]] =  [numberList[j],numberList[j-1]]
+        if type(i) == bool:
+            boolList.append(i)
+            for j in range(len(boolList)):
+                if (boolList[j] < boolList[j-1]):
+                    [boolList[j-1],boolList[j]] =  [boolList[j],boolList[j-1]]
+        if type(i) == str:
+            stringList.append(i)
+            for j in range(len(boolList)):
+                if (boolList[j] < boolList[j-1]):
+                    [boolList[j-1],boolList[j]] =  [boolList[j],boolList[j-1]]
+    return [numberList, stringList, boolList]         
+
+print(sortAllClean([ 1, 3, 'array', -45, True, False, 'big', None]))
