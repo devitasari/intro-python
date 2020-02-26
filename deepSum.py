c1835e0d54c6668edd23ee78d4d1c48cb4efb540
devitasari <devitas700@gmail.com>
2020-02-26 10:05:49 +0700

    array 3d

---
+++ deepSum.py
@@ -0,0 +1,50 @@
+def deepSum(arr):
+    if len(arr) == 0:
+        return 'No number'
+    output = 0
+    for i in arr:
+        for j in i:
+            for k in j:
+                output += k
+    return output
+
+print(deepSum([
+  [
+    [4, 5, 6],
+    [9, 1, 2, 10],
+    [9, 4, 3]
+  ],
+  [
+    [4, 14, 31],
+    [9, 10, 18, 12, 20],
+    [1, 4, 90]
+  ],
+  [
+    [2, 5, 10],
+    [3, 4, 5],
+    [2, 4, 5, 10]
+  ]
+])) # 316
+
+print(deepSum([
+  [
+    [20, 10],
+    [15],
+    [1, 1]
+  ],
+  [
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+    [2],
+    [9, 11]
+  ],
+  [
+    [3, 5, 1],
+    [1, 5, 3],
+    [1]
+  ],
+  [
+    [2]
+  ]
+])) # 156
+
+print(deepSum([])) # No number
